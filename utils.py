cfd45a464f3342194424fd0b212467826c77eaa6
filippobezzi <filippobezzi1@gmail.com>
2026-03-13 13:47:01 +0100

    Fixes

--- utils.py
+++ utils.py
@@ -34,32 +34,34 @@ class Reservoir:
         return states
 
 class DataGenerator:
-    def __init__(self, Time_steps, tau, n, beta, gamma, delta_t):
-        self.Time_steps = Time_steps
+    def __init__(self, Time_steps, tau, n, beta, gamma, delta_t, under_samp=10):
+        self.Time_steps = Time_steps*under_samp
+        self.under_samp = under_samp
         self.beta = beta
         self.gamma = gamma
         self.delta_t = delta_t
         self.tau = tau
         self.n = n
-        self.generate_data()
 
     def generate_data(self):
-        delay_steps = int(self.tau / self.delta_t)
+        delay_steps = int(self.tau / self.delta_t) * 10
         total_steps = self.Time_steps + delay_steps + 1
         x_history = np.zeros(total_steps+1)
-        x_history[:delay_steps] = 1.2 + np.random.uniform(-0.1, 0.1, delay_steps)
+        x_history[0], x_history[1] = 1.2 + np.random.uniform(-0.1, 0.1), 1.2 + np.random.uniform(-0.1, 0.1)
 
-        for t in range(delay_steps, total_steps, 1):
-            x_past = x_history[t - delay_steps]
+        for t in range(1, total_steps, 1):
+            x_past = x_history[t - 1]
             x_curr = x_history[t]
 
             x_succ = x_curr + ( self.beta * x_past / ( 1 + x_past**self.n )- self.gamma * x_curr ) * self.delta_t
 
             x_history[t + 1] = x_succ
-        X = x_history[delay_steps+1:-1]
-        X = (X - np.mean(X)) / np.std(X)
-        Y = x_history[delay_steps+2:]
-        Y = (Y - np.mean(Y)) / np.std(Y)
+
+        X = x_history[delay_steps+1:-1][::self.under_samp]
+        X = (X - np.mean(X)) / ( np.std(X) + 1e-8 )
+        Y = x_history[delay_steps+2:][::self.under_samp]
+        Y = (Y - np.mean(Y)) / ( np.std(Y) + 1e-8 )
+        print(delay_steps, x_history.shape, X.mean())
         return X.reshape((-1, 1)), Y.reshape((-1, 1))
     
 def get_reduced_states(Reservoir, X_train, X_test, n_components):
